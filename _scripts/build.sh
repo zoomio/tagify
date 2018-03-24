228e197cb5108fd22d7dcd6c33bdc34cf2dd4d43
smeshkov <smeshkow@gmail.com>
2018-03-24 20:43:23 +1100

    Optimizing CI

--- _scripts/build.sh
+++ _scripts/build.sh
@@ -8,7 +8,7 @@ fi
 
 go test -v ./...
 
-# use packr, to include files in binary"
+# use packr, to include files in binary
 go get -u github.com/gobuffalo/packr/...
 
 env GOOS=${OS} GOARCH=amd64 packr build -o tagify_${OS} cmd/cli/cli.go
---
+++ _scripts/lint.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+go get -u github.com/alecthomas/gometalinter
+gometalinter --install
+go install cmd/cli/cli.go
+gometalinter --fast --vendor ./...
