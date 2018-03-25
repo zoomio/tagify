4242687029cc753c210761cc8c319f9e7deb1764
smeshkov <smeshkow@gmail.com>
2018-03-25 14:03:10 +1100

    Use git tags in binary names, refined shell scripts a bit

--- _scripts/build.sh
+++ _scripts/build.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
 
+BINARY="tagify"
 OS="$1"
+VERSION="$2"
 
 if [ -z "$OS" ]; then
     OS="darwin"
 fi
 
-go test -v ./...
+if [ ! -z "$VERSION" ]; then
+    VERSION="_$VERSION"
+fi
 
 # use packr, to include files in binary
 go get -u github.com/gobuffalo/packr/...
 
-env GOOS=${OS} GOARCH=amd64 packr build -o tagify_${OS} cmd/cli/cli.go
+env GOOS=${OS} GOARCH=amd64 packr build -o ${BINARY}_${OS}${VERSION} cmd/cli/cli.go
--- _scripts/install.sh
+++ _scripts/install.sh
@@ -3,15 +3,21 @@
 BINARY="tagify"
 USER_BIN=$HOME/bin
 OS="$1"
+VERSION="$2"
 
 if [ -z "$OS" ]; then
     OS="darwin"
 fi
 
-chmod +x ${BINARY}_${OS}
+if [ ! -z "$VERSION" ]; then
+    VERSION="_$VERSION"
+fi
+
+chmod +x ${BINARY}_${OS}${VERSION}
 
 if [ ! -d "$USER_BIN" ]; then
   mkdir -p ${USER_BIN}
+  echo "created $USER_BIN directory, don't forget to add it to PATH environment variable"
 fi
 
-mv ${BINARY}_${OS} ${USER_BIN}/${BINARY}
+mv ${BINARY}_${OS}${VERSION} ${USER_BIN}/${BINARY}
