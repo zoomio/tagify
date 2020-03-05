ee2dca6dbb58a58dd9c98ecb515b67075cc14aa3
smeshkov <smeshkow@gmail.com>
2020-03-05 12:24:29 +1100

    Updated `_bin/install.sh` to get latest release from GH and install into the user bin

--- _bin/install.sh
+++ _bin/install.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-DIST_DIR="_dist"
 BINARY="tagify"
 USER_BIN=$HOME/bin
 OS="$1"
@@ -14,14 +13,16 @@ if [ ! -z "$VERSION" ]; then
     VERSION="_$VERSION"
 fi
 
-file="${VERSION}/${BINARY}_${OS}_${VERSION}"
+link=$(curl -s https://api.github.com/repos/jgm/pandoc/releases/latest | grep "browser_download_url.*deb" | cut -d : -f 2,3 | tr -d \")
 
-curl -O "https://github.com/zoomio/tagify/releases/download/${file}"
-chmod +x ${file}
+curl -o ${BINARY} ${link}
+chmod +x ${BINARY}
 
 if [ ! -d "$USER_BIN" ]; then
   mkdir -p ${USER_BIN}
   echo "created $USER_BIN directory, don't forget to add it to PATH environment variable"
 fi
 
-mv ${file} ${USER_BIN}/${BINARY}
+mv ${BINARY} ${USER_BIN}/${BINARY}
+
+echo "Installation is done."
