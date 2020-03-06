39ac444eddfcb9e0c69371c29608129102754ba4
smeshkov <smeshkow@gmail.com>
2020-03-06 12:53:13 +1100

    Updated installer script so it is actually working now

--- _bin/install.sh
+++ _bin/install.sh
@@ -10,7 +10,9 @@ fi
 
 link=$(curl -s https://api.github.com/repos/zoomio/tagify/releases/latest | grep "browser_download_url.*tagify_${OS}" | cut -d : -f 2,3 | tr -d \")
 
-curl -o ${BINARY} ${link}
+echo "downloading ${BINARY} from $link"
+
+curl -L -o ${BINARY} ${link}
 chmod +x ${BINARY}
 
 if [ ! -d "$USER_BIN" ]; then
@@ -18,6 +20,8 @@ if [ ! -d "$USER_BIN" ]; then
   echo "created $USER_BIN directory, don't forget to add it to PATH environment variable"
 fi
 
+echo "moving ${BINARY} to ${USER_BIN}/${BINARY}"
+
 mv ${BINARY} ${USER_BIN}/${BINARY}
 
-echo "Installation is done."
+echo "installation is done."
